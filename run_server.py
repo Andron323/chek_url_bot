75073e41aed7cf5cdefb073488216da1a2f2b115
Andron323 <opamil323andron@gmail.com>
2020-03-21 19:18:16 +0200

    first commit on Heroku

---
+++ run_server.py
@@ -0,0 +1,29 @@
+import flask
+import telebot
+from telebot import types
+
+import constants
+from constants import *
+import os
+
+server = flask.Flask(__name__)
+
+bot = telebot.TeleBot(constants.token)
+
+
+@server.route('/' + token, methods=['POST'])
+def get_message():
+    bot.process_new_updates([types.Update.de_json(
+        flask.request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route('/', methods=["GET"])
+def index():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://{}.herokuapp.com/{}".format(APP_NAME, token))
+    return "Hello from Heroku!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
